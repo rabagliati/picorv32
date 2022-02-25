8edc48f585df799a65f8030d9aaec448b126379b
Andy Rabagliati <andyr@wizzy.com>
2022-02-25 23:45:47 +0200

    add uart

--- scripts/vivado/system.v
+++ scripts/vivado/system.v
@@ -15,11 +15,12 @@ module system (
 
 	wire mem_valid;
 	wire mem_instr;
-	reg mem_ready;
+        reg ram_ready;
 	wire [31:0] mem_addr;
 	wire [31:0] mem_wdata;
 	wire [3:0] mem_wstrb;
-	reg [31:0] mem_rdata;
+	reg [31:0] ram_rdata;
+	wire  [31:0] mem_rdata = ram_rdata;
 
 	wire mem_la_read;
 	wire mem_la_write;
@@ -27,6 +28,21 @@ module system (
 	wire [31:0] mem_la_wdata;
 	wire [3:0] mem_la_wstrb;
 
+	wire        simpleuart_reg_div_sel = mem_valid && (mem_addr == 32'h 0200_0004);
+	wire [31:0] simpleuart_reg_div_do;
+
+	wire        simpleuart_reg_dat_sel = mem_valid && (mem_addr == 32'h 0200_0008);
+	wire [31:0] simpleuart_reg_dat_do;
+
+        wire   simpleuart_reg_dat_wait;
+
+	assign mem_ready = ram_ready ||
+			   simpleuart_reg_div_sel ||
+                     (simpleuart_reg_dat_sel && !simpleuart_reg_dat_wait);
+
+	assign mem_rdata = ram_ready ? ram_rdata :
+                           simpleuart_reg_div_sel ? simpleuart_reg_div_do :
+                           simpleuart_reg_dat_sel ? simpleuart_reg_dat_do : 32'h 0000_0000;
 	picorv32 picorv32_core (
 		.clk         (clk         ),
 		.resetn      (resetn      ),
@@ -45,6 +61,24 @@ module system (
 		.mem_la_wstrb(mem_la_wstrb)
 	);
 
+	simpleuart simpleuart (
+		.clk         (clk         ),
+		.resetn      (resetn      ),
+
+		.ser_tx      (ser_tx      ),
+		.ser_rx      (ser_rx      ),
+
+		.reg_div_we  (simpleuart_reg_div_sel ? mem_wstrb : 4'b 0000),
+		.reg_div_di  (mem_wdata),
+		.reg_div_do  (simpleuart_reg_div_do),
+
+		.reg_dat_we  (simpleuart_reg_dat_sel ? mem_wstrb[0] : 1'b 0),
+		.reg_dat_re  (simpleuart_reg_dat_sel && !mem_wstrb),
+		.reg_dat_di  (mem_wdata),
+		.reg_dat_do  (simpleuart_reg_dat_do),
+		.reg_dat_wait(simpleuart_reg_dat_wait)
+	);
+
 	reg [31:0] memory [0:MEM_SIZE-1];
 	initial $readmemh("firmware.hex", memory);
 
@@ -53,9 +87,9 @@ module system (
 
 	generate if (FAST_MEMORY) begin
 		always @(posedge clk) begin
-			mem_ready <= 1;
+			ram_ready <= 1;
 			out_byte_en <= 0;
-			mem_rdata <= memory[mem_la_addr >> 2];
+			ram_rdata <= memory[mem_la_addr >> 2];
 			if (mem_la_write && (mem_la_addr >> 2) < MEM_SIZE) begin
 				if (mem_la_wstrb[0]) memory[mem_la_addr >> 2][ 7: 0] <= mem_la_wdata[ 7: 0];
 				if (mem_la_wstrb[1]) memory[mem_la_addr >> 2][15: 8] <= mem_la_wdata[15: 8];
@@ -71,29 +105,29 @@ module system (
 	end else begin
 		always @(posedge clk) begin
 			m_read_en <= 0;
-			mem_ready <= mem_valid && !mem_ready && m_read_en;
+			ram_ready <= mem_valid && !ram_ready && m_read_en;
 
 			m_read_data <= memory[mem_addr >> 2];
-			mem_rdata <= m_read_data;
+			ram_rdata <= m_read_data;
 
 			out_byte_en <= 0;
 
 			(* parallel_case *)
 			case (1)
-				mem_valid && !mem_ready && !mem_wstrb && (mem_addr >> 2) < MEM_SIZE: begin
+				mem_valid && !ram_ready && !mem_wstrb && (mem_addr >> 2) < MEM_SIZE: begin
 					m_read_en <= 1;
 				end
-				mem_valid && !mem_ready && |mem_wstrb && (mem_addr >> 2) < MEM_SIZE: begin
+				mem_valid && !ram_ready && |mem_wstrb && (mem_addr >> 2) < MEM_SIZE: begin
 					if (mem_wstrb[0]) memory[mem_addr >> 2][ 7: 0] <= mem_wdata[ 7: 0];
 					if (mem_wstrb[1]) memory[mem_addr >> 2][15: 8] <= mem_wdata[15: 8];
 					if (mem_wstrb[2]) memory[mem_addr >> 2][23:16] <= mem_wdata[23:16];
 					if (mem_wstrb[3]) memory[mem_addr >> 2][31:24] <= mem_wdata[31:24];
-					mem_ready <= 1;
+					ram_ready <= 1;
 				end
-				mem_valid && !mem_ready && |mem_wstrb && mem_addr == 32'h1000_0000: begin
+				mem_valid && !ram_ready && |mem_wstrb && mem_addr == 32'h1000_0000: begin
 					out_byte_en <= 1;
 					out_byte <= mem_wdata;
-					mem_ready <= 1;
+					ram_ready <= 1;
 				end
 			endcase
 		end
